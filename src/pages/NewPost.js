4d7e1e94d27490a6a3ad581cfa4134da0ffee9bf
Raunak <reachtoraunak@gmail.com>
2021-05-29 00:23:11 +0530

    NewPostForm component, and firebase integration :sparkles:

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,5 +1,25 @@
+import NewPostForm from "../component/posts/NewPostForm";
+
 function NewPostPage() {
-    return <div>New Post Page</div>;
+    function addPostHandler(postData) {
+        fetch(
+            "https://my-blog-ce0b4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
+            {
+                method: "POST",
+                body: JSON.stringify(postData),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+    }
+
+    return (
+        <div>
+            <h2>New Post Page</h2>
+            <NewPostForm onAddPost={addPostHandler} />
+        </div>
+    );
 }
 
 export default NewPostPage;
