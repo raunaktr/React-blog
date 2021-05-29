502b058a2853f640239006119f83af92fa412b27
Raunak <reachtoraunak@gmail.com>
2021-05-29 18:30:33 +0530

    adding useState & useEffect hooks in AllPost :sparkles:

--- src/pages/AllPosts.js
+++ src/pages/AllPosts.js
@@ -1,26 +1,45 @@
+import { useState, useEffect } from "react";
 import PostList from "../component/posts/PostList";
 
-const DUMMY_DATA = [
-    {
-        id: "t1",
-        title: "This is first title",
-        image: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-        description: "This is first description",
-    },
-    {
-        id: "t2",
-        title: "This is second title",
-        image: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-        description: "This is second description",
-    },
-];
-
 function AllPostsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedPosts, setLoadedPosts] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+            "https://my-blog-ce0b4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const posts = [];
+                for (const key in data) {
+                    const post = {
+                        id: key,
+                        ...data[key],
+                    };
+                    posts.push(post);
+                }
+                setIsLoading(false);
+                setLoadedPosts(posts);
+            });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
     return (
-        <div>
+        <section>
             <h1>All Posts Page</h1>
-            <PostList posts={DUMMY_DATA} />
-        </div>
+            <PostList posts={loadedPosts} />
+        </section>
     );
 }
 export default AllPostsPage;
