b6fdf23a1e8c8d201421ddc9e59a1a234bf537f6
Raunak <reachtoraunak@gmail.com>
2021-05-29 17:13:22 +0530

    using useHistory hook, i.e programmatic navigation :sparkles:

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,6 +1,9 @@
+import { useHistory } from "react-router-dom";
 import NewPostForm from "../component/posts/NewPostForm";
 
 function NewPostPage() {
+    const history = useHistory();
+    
     function addPostHandler(postData) {
         fetch(
             "https://my-blog-ce0b4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
@@ -11,7 +14,9 @@ function NewPostPage() {
                     "Content-Type": "application/json",
                 },
             }
-        );
+        ).then(() => {
+            history.replace("/");
+        });
     }
 
     return (
